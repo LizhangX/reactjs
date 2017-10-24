987b19ecda681062d2be6748b7376214221ebbb2
Lizhang_Xie <lizhang.xie@gmail.com>
2017-10-23 22:24:15 -0500

    adding react-icons

--- React_Assignments/Exercise Files/Ch03/03_02/start/src/components/GymDayCount.js
+++ React_Assignments/Exercise Files/Ch03/03_02/start/src/components/GymDayCount.js
@@ -1,25 +1,33 @@
 import '../stylesheets/ui.scss';
+// import Terrain from 'react-icons/lib/md/terrain';
+// import SnowFlake from 'react-icons/lib/ti/weather-snow';
+import Calendar from 'react-icons/lib/fa/calendar';
+import TiWeatherShower from 'react-icons/lib/ti/weather-shower';
+import MdEqualizer from 'react-icons/lib/md/equalizer';
 
 const percentToDecimal = (decimal) => {
-    return ((decimal * 100) + '%')
-}
+    return ((decimal * 100) + '%');
+};
 
 const calGoalProgress = (total, goal) => {
-    return percentToDecimal(total/goal)
-}
+    return percentToDecimal(total/goal);
+};
 
 export const GymDayCount = ({total, rain, busy, goal}) => (
         <div className="gym-day-count">
             <div className="total-days">
                 <span>{total} </span>
+                <Calendar />                
                 <span>days</span>
             </div>
             <div className="rain-days">
                 <span>{rain} </span>
+                <TiWeatherShower />
                 <span>days</span>
             </div>
             <div className="busy-days">
                 <span>{busy} </span>
+                <MdEqualizer />
                 <span>days</span>
             </div>
             <div>
